a68c92408ad56658bccc31bc64dad4136178c211
Michael Fischer <michael.fischer3@gmx.de>
2021-12-03 20:16:02 +0100

    .

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gascomp',
-    version='1.0.0',
+    version='1.0.1',
     description='GasComp',
     long_description='GasComp',
     author='Michael Fischer',
